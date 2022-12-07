f31deb1d9ee812ba29733faa15cc360b0be2e37a
ariabagheri <ariab9342@gmail.com>
2022-12-06 19:21:29 -0500

    Initial Commit

--- src/components/home/StarsVideoBackground.tsx
+++ src/components/home/StarsVideoBackground.tsx
@@ -4,7 +4,7 @@ import styles from "../../../styles/components/home/StarsVideoBackground.module.
 function StarsVideoBackground({children}: {children: React.ReactNode}) {
     return (
         <>
-            <video className={styles.video} src={"/home_video.mp4"} width={"100%"} height={"100%"} muted autoPlay loop/>
+            <video className={styles.video} src={"https://ariabagheristorage.s3.amazonaws.com/home_video.mp4"} width={"100%"} height={"100%"} muted autoPlay loop/>
             <div className={styles.videoOverlay}>
                 {children}
             </div>
