ddd56670539779c63b4b56e18a1c5bf6e5f96772
ariabagheri <ariab9342@gmail.com>
2022-12-07 02:40:56 -0500

    responsive header added

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,8 @@ export default function Head() {
     return (
         <>
             <title>Aria Bagheri</title>
+                        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+
         </>
     )
 }
